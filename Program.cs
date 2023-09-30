b36422283514c2b6c14e38a698d099f8cf5163d6
Ali Salmeh <alisalmeh18@gmail.com>
2023-09-30 18:39:00 +0330

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek2_Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the distance in miles: ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+
+            // process converting miles to kilometers
+            double convertMilesToKilometers = Math.Round((userInput / 0.621371), 5);
+
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{userInput} miles is equal to {convertMilesToKilometers} kilometers.");
+            Console.ResetColor();
+        }
+    }
+}
